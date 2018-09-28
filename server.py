e09c3d928ff00c348b354359a0a9626a060e5d4b
Kleymenova Olga <ok.greatteacher@gmail.com>
2018-09-28 18:46:47 +0300

    переместила коды погоды в сеттингс, а их спрятала от гит хаба

--- server.py
+++ server.py
@@ -4,24 +4,24 @@ from flask import Flask
 
 from req import get_weather
 
-city_id = 524901
+import settings
 
-apikey = '07535016bb8491634911732e9a804516'
+city_id = 524901
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	url = "http://api.openweathermap.org/data/2.5/weather?id=%s&APPID=%s&units=metric" % (city_id, apikey)
+	url = "http://api.openweathermap.org/data/2.5/weather?id=%s&APPID=%s&units=metric" % (city_id, settings.API_KEY)
 	weather = get_weather(url)
 	cur_date = datetime.now().strftime(' %A  %d %B')
-	result = "<p> температура за бортом: %s </p>" % weather ['main']['temp']
-	result += "<p>  <b> мы летим над городом: </b> %s </p>" % weather['name']
+	result = "<p> Tемпература за бортом: %s </p>" % weather ['main']['temp']
+	result += "<p>  <b> Mы летим над городом: </b> %s </p>" % weather['name']
 	result += "<p> Today is : %s </p>" % cur_date
 	return result
 
 
 if __name__=="__main__":
-	app.run()
+	app.run(port=5010)      # если что, d скобках стереть порт равен 5010 или использовать что-то другое, автоматически использует 5000
 
 
