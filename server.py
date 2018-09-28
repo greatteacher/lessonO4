180c17688e7595e32a15039d1781559e3536cd2d
Kleymenova Olga <ok.greatteacher@gmail.com>
2018-09-28 20:27:06 +0300

    как новость добавлять на разные странички

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from flask import Flask
-
+from all_news_list import all_news
 from req import get_weather
 
 import settings
@@ -19,6 +19,20 @@ def index():
 	result += "<p>  <b> Mы летим над городом: </b> %s </p>" % weather['name']
 	result += "<p> Today is : %s </p>" % cur_date
 	return result
+
+@app.route("/news/<int:news_id>")
+def news_by_id(news_id):
+	# date = date.strftime(' %m %Y %B') попыталась дату формат поменять.. надо подумать как
+	news_to_show = [news for news in all_news if news['id'] == news_id]
+	# return 'Новость:  ' + str(news_id) # сделали строкой, попросили только число выводить
+	if len(news_to_show) == 1:
+		# print(news_to_show) # выводил новость 
+		result = "<h1>%(title)s</h1><p><i>%(date)s</i></p><p><b>%(text)s</i></b>"
+		result = result % news_to_show[0]
+		return 'Новость:  %s' % result # новость загала в ковычки, заменив подстановокой, т.е. %
+	else: 
+		abort(404)
+
 
 
 if __name__=="__main__":
